f617004de9ca671046b25f9f57e009ff1cb9f68d
zac1st1k <zacfirst@gmail.com>
2014-12-15 19:56:54 +1100

    Initial Commit

---
+++ OverdueTests/OverdueTests.m
@@ -0,0 +1,34 @@
+//
+//  OverdueTests.m
+//  OverdueTests
+//
+//  Created by Zac on 15/12/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OverdueTests : XCTestCase
+
+@end
+
+@implementation OverdueTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
